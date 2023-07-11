63a16a7171e82d502e4cf775c794e0ecd96fec0e
JannisK89 <jannis.karanikis@gmail.com>
2023-07-11 15:17:44 +0200

    day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,6 +4,7 @@ use utils;
 
 fn main() {
     run_day1();
+    run_day2();
 }
 
 fn run_day1() {
@@ -13,5 +14,15 @@ fn run_day1() {
     });
     let split = utils::split_input(&input);
     let total = day1::calculate_calories(split);
-    println!("Day 1, puzzle 1: {total}");
+    println!("Day 1, part 1: {total}");
+}
+
+fn run_day2() {
+    let input = fs::read_to_string("./input.txt").unwrap_or_else(|error| {
+        eprintln!("{:?}", error);
+        process::exit(1);
+    });
+    let split = utils::split_input(&input);
+    let total = day1::calculate_top_three_calories(split);
+    println!("Day 1, part 2: {total}");
 }
