167c5eed4b359f0f2421fabe3d9a8802442daded
JannisK89 <jannis.karanikis@gmail.com>
2023-07-23 19:52:22 +0200

    day 3 part 2

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -11,15 +11,55 @@ pub fn find_letter(line: &str) -> Option<char> {
     None
 }
 
-pub fn calculate_letter_value(letter:&char) -> u32 {
-        let value = *letter as u32;
-        let capital = letter.is_uppercase();
+pub fn calculate_letter_value(letter: &char) -> u32 {
+    let value = *letter as u32;
+    let capital = letter.is_uppercase();
 
-        if capital {
-           return value-38; 
-        } else {
-            return value-96
+    if capital {
+        return value - 38;
+    } else {
+        return value - 96;
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct GroupOfThree<'a> {
+    first: &'a str,
+    second: &'a str,
+    third: &'a str,
+}
+
+impl<'a> GroupOfThree<'a> {
+    pub fn new(first: &'a str, second: &'a str, third: &'a str) -> GroupOfThree<'a> {
+        GroupOfThree {
+            first,
+            second,
+            third,
         }
+    }
+
+    pub fn find_common(&self) -> Option<char> {
+        for letter in self.first.chars() {
+            if self.second.contains(letter) && self.third.contains(letter) {
+                return Some(letter);
+            }
+        }
+        None
+    }
+}
+
+pub fn split_into_groups<'a>(split_text: &'a Vec<&'a str>) -> Vec<GroupOfThree<'a>> {
+    let mut iterator = split_text.iter();
+    let mut groups: Vec<GroupOfThree> = vec![];
+    for _ in 0..split_text.len() / 3 {
+        let group = GroupOfThree::new(
+            iterator.next().unwrap(),
+            iterator.next().unwrap(),
+            iterator.next().unwrap(),
+        );
+        groups.push(group);
+    }
+    groups
 }
 
 #[cfg(test)]
@@ -53,5 +93,51 @@ mod tests {
             let score = calculate_letter_value(&reoccuring_letters[index]);
             assert_eq!(score, score_vec[index]);
         }
+    }
+
+    #[test]
+    fn returns_letter_available_in_whole_group() {
+        let group_one = GroupOfThree::new(
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+        );
+
+        let group_two = GroupOfThree::new(
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw",
+        );
+
+        let letter_one = group_one.find_common().unwrap();
+        let letter_two = group_two.find_common().unwrap();
+
+        assert_eq!(letter_one, 'r');
+        assert_eq!(letter_two, 'Z');
+    }
+    #[test]
+    fn returns_groups_of_three() {
+        let group_vec = vec![
+            "Test1.1", "Test1.2", "Test1.3", "Test2.1", "Test2.2", "Test2.3",
+        ];
+
+        let groups = split_into_groups(&group_vec);
+
+        assert_eq!(
+            groups[0],
+            GroupOfThree {
+                first: "Test1.1",
+                second: "Test1.2",
+                third: "Test1.3"
+            }
+        );
+        assert_eq!(
+            groups[1],
+            GroupOfThree {
+                first: "Test2.1",
+                second: "Test2.2",
+                third: "Test2.3"
+            }
+        );
     }
 }
--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,6 +4,7 @@ use utils;
 
 fn main() {
     run_one();
+    run_two();
 }
 
 fn run_one() {
@@ -29,5 +30,24 @@ fn run_one() {
         let value = day3::calculate_letter_value(letter);
         total += value;
     }
+    println!("{total}");
+}
+
+fn run_two() {
+    let mut total: u32 = 0;
+    let input = fs::read_to_string("./input.txt").unwrap_or_else(|error| {
+        eprintln!("{:?}", error);
+        process::exit(1);
+    });
+    let split = utils::split_input(&input);
+
+    let groups = day3::split_into_groups(&split);
+    for group in groups {
+        let score = group
+            .find_common()
+            .unwrap_or_else(|| panic!("Could not find common letter"));
+        total += day3::calculate_letter_value(&score);
+    }
+
     println!("{total}");
 }
